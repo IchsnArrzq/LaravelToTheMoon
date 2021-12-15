45c4e89494760aef881604cf2a5e41e8d4e83e03
IchsnArrzq <ichsanarrizqi@smkwikrama.sch.id>
2021-12-15 08:43:35 +0000

    first commit from google cloud

---
+++ app/Http/Controllers/FirebaseController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Kreait\Firebase;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+use Kreait\Firebase\Database;
+class FirebaseController extends Controller
+{
+    public function index(){
+        $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/laravel-to-the-moon-firebase-adminsdk-dn7fb-b0c7ad5112.json');
+        $firebase = (new Factory)
+            ->withServiceAccount($serviceAccount)
+            ->create();
+        $database = $firebase->getDatabase();
+        $ref = $database->getReference('Subjects');
+        $key = $ref->push()->getKey();
+        $ref->getChild($key)->set([
+            'SubjectName' => 'Laravel'
+        ]);
+        return $key;
+    }
+}
